af34d0263f081848814c9b50929a37f234e78065
Kaari Strack <kaari@absolutedevops.io>
2018-05-10 11:05:46 +0100

    :construction: added edit, update, and destroy functionality

--- Tixit/app/controllers/tasks_controller.rb
+++ Tixit/app/controllers/tasks_controller.rb
@@ -25,9 +25,16 @@ before_action :find_task, only: [:show, :edit, :update, :destroy]
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to root_path
   end
 
   private
